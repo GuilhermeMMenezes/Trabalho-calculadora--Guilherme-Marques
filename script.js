98f7df327c5ab6359773579480c3f6b00c74d4ed
GuilhermeMMenezes <137406003+GuilhermeMMenezes@users.noreply.github.com>
2024-05-24 18:32:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const resultado = document.getElementById(`result`)
 
 function clicar(char){
-    if(resultado.innerText == `0` && char != `.`){
+    if(resultado.innerText == `0` && char != `.`){ //Aqui é pra quando for apertar algum numero, o zero inicial sair, para que apareça o numero escolhido
         resultado.innerText = char
     }else {
-        if(!(char == `.` && resultado.innerText[resultado.innerText.length - 1] == `.`)){
+        if(!(char == `.` && resultado.innerText[resultado.innerText.length - 1] == `.`)){ //Aqui é pra limitar a quantia de pontos
             resultado.innerText += char
         }
     }
 }
 function limpar(){
-    resultado.innerHTML = `0`
+    resultado.innerHTML = `0`  //Aqui é pra quando apertar no botão limpar, os numeros voltem a zero (numero inicial)
 }
 function resultadoFinal (){
     // resultado.innerHTML = `Hello World!`
-    resultado.innerHTML = eval(resultado.innerText)
+    resultado.innerHTML = eval(resultado.innerText) //Aqui é pra aparecer o resultado
 }
